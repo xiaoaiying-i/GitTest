4922385191a677e20078d036dd98e6e0bbe62f1a
xiaoai <1944408285@qq.com>
2020-11-28 01:37:31 +0800

    分支2-helloworld2类添加一行输出

--- src/main/java/com/xiaoai/Hlelloworld2.java
+++ src/main/java/com/xiaoai/Hlelloworld2.java
@@ -3,5 +3,8 @@ package com.xiaoai;
 public class Hlelloworld2 {
     public static void main(String[] args){
         System.out.println("helloworld2!!,我是分支2");
+        System.out.println("helloworld2!!,我是分支2");
+
+
     }
 }
